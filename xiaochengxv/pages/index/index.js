b7abf1ddf0e8a2687a91bb837ce2a7add348b95d
tswowo6 <tswossion@outlook.com>
2026-03-23 19:54:20 +0800

    feature: 小程序开发测试

--- xiaochengxv/pages/index/index.js
+++ xiaochengxv/pages/index/index.js
@@ -1,49 +1,54 @@
-// index.js
-const defaultAvatarUrl = 'https://mmbiz.qpic.cn/mmbiz/icTdbqWNOwNRna42FI242Lcia07jQodd2FJGIYQfG0LAJGFxM4FbnQP6yfMxBgJ0F3YRqJCJ1aPAK2dQagdusBZg/0'
-
+// pages/index/index.js
 Page({
   data: {
-    motto: 'Hello World',
     userInfo: {
-      avatarUrl: defaultAvatarUrl,
       nickName: '',
+      gender: '',
+      avatarUrl: '',
     },
-    hasUserInfo: false,
-    canIUseGetUserProfile: wx.canIUse('getUserProfile'),
-    canIUseNicknameComp: wx.canIUse('input.type.nickname'),
+    loginCode:'',
+    shopStatus:''
   },
-  bindViewTap() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
-  onChooseAvatar(e) {
-    const { avatarUrl } = e.detail
-    const { nickName } = this.data.userInfo
-    this.setData({
-      "userInfo.avatarUrl": avatarUrl,
-      hasUserInfo: nickName && avatarUrl && avatarUrl !== defaultAvatarUrl,
-    })
-  },
-  onInputChange(e) {
-    const nickName = e.detail.value
-    const { avatarUrl } = this.data.userInfo
-    this.setData({
-      "userInfo.nickName": nickName,
-      hasUserInfo: nickName && avatarUrl && avatarUrl !== defaultAvatarUrl,
-    })
-  },
-  getUserProfile(e) {
-    // 推荐使用wx.getUserProfile获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认，开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
+
+  //请求用户信息
+  getUserProfile() {
     wx.getUserProfile({
-      desc: '展示用户信息', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
+      desc: '用于展示信息',
       success: (res) => {
         console.log(res)
         this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
+          userInfo: res.userInfo
+        })
+      },
+      fail:(error) => {
+        console.log(error);
+      }
+    })
+  },
+
+  // 微信登陆，获取微信用户授权码
+  wxLogin(){
+    wx.login({
+      success:(res)=>{
+        console.log(res);
+        this.setData({
+          code:res.code
         })
       }
     })
   },
+
+  //发送请求
+  sendRequest(){
+    wx.request({
+      url:'http://localhost:8080/user/shop/status',
+      method:'GET',
+      success:(res)=>{
+        console.log(res.data.data);
+        this.setData({
+            shopStatus:res.data.data
+        })
+      }
+    })
+  }
 })
