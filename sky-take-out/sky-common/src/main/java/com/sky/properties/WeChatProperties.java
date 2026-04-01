ec23e1ee2fd0c29b3de02afa1f09e41ec8629449
tswowo6 <tswossion@outlook.com>
2026-04-01 21:44:11 +0800

    fix: 删除了模拟支付的冗余字段

--- sky-take-out/sky-common/src/main/java/com/sky/properties/WeChatProperties.java
+++ sky-take-out/sky-common/src/main/java/com/sky/properties/WeChatProperties.java
@@ -14,12 +14,5 @@ public class WeChatProperties {
     private String appid; //小程序的appid
     @Value("${sky.wechat.app-secret}")
     private String secret; //小程序的秘钥
-    private String mchid; //商户号
-    private String mchSerialNo; //商户API证书的证书序列号
-    private String privateKeyFilePath; //商户私钥文件
-    private String apiV3Key; //证书解密的密钥
-    private String weChatPayCertFilePath; //平台证书
-    private String notifyUrl; //支付成功的回调地址
-    private String refundNotifyUrl; //退款成功的回调地址
 
 }
